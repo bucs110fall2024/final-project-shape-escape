3fc0f3ab1abf6ab3e665dbb14f21bdfd43f6af89
Jimmy Zou <Jimz7192@gmail.com>
2024-11-20 17:33:32 -0500

    pusj

--- .github/src/Triangle.py
+++ .github/src/Triangle.py
@@ -1,13 +1,32 @@
-class Triangle:
+import pygame
+import sys
 
-  def __init__(self, x, y, img_file):
-      self.score = 0
-      self.tri.x = x
-      self.tri.y = y
+pygame.init()
 
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("GAME")
 
-  def move_right(self):
-     self.tri.x += 1
+clock = pygame.time.Clock()
 
-  def move_left(self):
-      self.tri.x -=1
+class Triangle(pygame.sprite.Sprite):
+      def __init__(self, x, y, img="assets/triangle.png"):
+        super().__init__()
+
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+      def right(self, distance=5):
+            self.rect.x += distance
+            
+      def left(self, distance=5):
+            self.rect.x -= distance
+
+      def update(self):
+        if self.rect.x > width:
+            self.rect.x = -50  
+        elif self.rect.x < -50:
+            self.rect.x = width
+
