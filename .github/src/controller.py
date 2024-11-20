dc98bff2471f7b90a71e0ca9d157eb712272608c
Jimmy Zou <Jimz7192@gmail.com>
2024-11-20 18:57:37 -0500

    commit

--- .github/src/controller.py
+++ .github/src/controller.py
@@ -1,19 +1,36 @@
 import pygame
-import Triangle
-import Square
-import Game
-import Bullet
+from Triangle import Triangle
+from Square import Square
+from Game import Game
+from Bullet import Bullet
 
-def mainloop(self):
-   while(True): 
-      for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-               pygame.quit()
-               exit()
+class Controller:
+    def __init__(self):
+        pygame.init()
+        pygame.event.pump()
 
-      #2. detect collisions and update models
+        self.screen = pygame.display.set_mode()
+        
+        triangle = Triangle(0,0)
 
-      #3. Redraw next frame
+    def mainloop(self):
+        while(True): 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.triangle.left()
+                    if event.key == pygame.K_RIGHT:
+                        self.triangle.right()
+                    if event.key == pygame.K_DOWN:
+                        self.triangle.down()
+                    if event.key == pygame.K_UP:
+                        self.triangle.up()
+            #2. detect collisions and update models
 
-      #4. Display next frame
-      pygame.display.flip()
+            #3. Redraw next frame
+
+            #4. Display next frame
+            pygame.display.flip()
